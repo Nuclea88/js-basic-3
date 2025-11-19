450318e01e1524023b2a9c013ea81bfed6df4b43
nucleaG <nuclea.g@gmail.com>
2025-11-19 14:32:42 +0100

    ejercicio realizado

---
+++ js-basic-3.js
@@ -0,0 +1,57 @@
+
+
+//Números:
+
+//1. Escribe una función que tome dos números como argumentos y devuelva su suma.
+function sumar(a, b) {
+    return a+b;
+}
+
+//2. Escribe una función que tome dos números como argumentos y devuelva su resta.
+function restar(a, b) {
+    return a-b;
+}
+//3. Escribe una función que tome dos números como argumentos y devuelva su producto.
+function multiplicar(a, b) {
+    return a*b;
+}
+//4. Escribe una función que tome dos números como argumentos y devuelva su división.
+function dividir (a,b){
+    return a/b;
+}
+//5. Escribe una función que tome un número base y un exponente y devuelva el resultado de elevar el número base a la potencia dada.
+function potencia(base, exponente) {
+    return a**b;
+}
+
+//6. Escribe una función que tome dos números como argumentos y devuelva el resto de la división del primer número entre el segundo.
+function rest (a,b){
+
+    return a%b;
+}
+
+//7. Escribe una función que tome un número como argumento y devuelva su raíz cuadrada.
+function raizCuadrada(a){
+    return Math.sqrt(a);
+}
+
+//8. Escribe una función que tome un número como argumento y devuelva su valor absoluto.
+function absolute (a){
+
+    return Math.abs(a);
+}
+//9. Escribe una función que tome un número decimal como argumento y devuelva el número redondeado al entero más cercano.
+function redondeo(a){
+
+    return Math.round(a);
+}
+
+//10. Escribe una función que genere y devuelva un número aleatorio entre 0 y 1.
+
+function randomNumber (){
+
+    return Math.random();
+}
+
+
+
